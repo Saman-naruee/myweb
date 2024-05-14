f75da876aeeac6eddf621de050358db9784331f6
tacode01 <tacode01@hq-tacode-001.digikala.COM>
2024-05-14 21:24:56 +0330

    add canonical urls to templates and models.py

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django_jalali.db import models as jmodels
+from django.urls import reverse
 
 
 # Managers
@@ -46,4 +47,7 @@ class Post(models.Model):
 
     def __str__(self) -> str:
         return self.Title
+    
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args= [str(self.id)])
     
