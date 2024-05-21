39fdd4f8c2574905f0b7cdbd21752344d7620cc7
tacode01 <tacode01@hq-tacode-001.digikala.COM>
2024-05-22 02:12:44 +0330

    make paginations for pages

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404
 from . models import Post
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
 
@@ -9,6 +10,14 @@ def index(request):
 
 def post_list(request):
     posts = Post.Published.all()
+    paginator = Paginator(posts, 1)
+    page_num = request.GET.get('page', 1)
+    try: 
+        posts = paginator.page(page_num)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
     context = {
         'posts' : posts,
     }
@@ -21,7 +30,6 @@ def post_detail(request, id):
     #     post = Post.Published.get(id = id) 
     # except:
     #     raise Http404("No Post Found!")
-    
     context = {
         'post' : post,
     }
