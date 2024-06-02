d3ca8981d1f358f766f71a8c61a0465bb2c7a55f
tacode01 <tacode01@hq-tacode-001.digikala.COM>
2024-06-02 15:58:38 +0330

    turning out the views to OOP form functional, get last commit to dont changes

--- blog/views.py
+++ blog/views.py
@@ -2,27 +2,27 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404
 from . models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView, DeleteView
+
 
 # Create your views here.
 
 def index(request):
     return HttpResponse("index")
 
-def post_list(request):
-    posts = Post.Published.all()
-    paginator = Paginator(posts, 1)
-    page_num = request.GET.get('page', 1)
-    try: 
-        posts = paginator.page(page_num)
-    except EmptyPage:
-        posts = paginator.page(paginator.num_pages)
-    except PageNotAnInteger:
-        posts = paginator.page(1)
-    context = {
-        'posts' : posts,
-    }
-    return render(request, "blog/list.html", context)
+
+class PostListView(ListView):
+    # model = Post
+    context_object_name = 'posts'
+    queryset = Post.Published.all()
+    paginate_by = 3
+    template_name = 'blog/list.html'
     
+
+class PostDetailView(DeleteView):
+    queryset = Post.Published.all()
+    template_name = "blog/detail.html"
+
 
 def post_detail(request, id):
     post = get_object_or_404(Post, id = id, status = Post.Status.PUBLISHED)
@@ -34,5 +34,4 @@ def post_detail(request, id):
         'post' : post,
     }
     return render(request, "blog/detail.html", context)
-    
     
