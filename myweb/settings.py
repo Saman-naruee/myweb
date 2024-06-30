21ecc3061908831936fdaaafb64478a5314c7067
tacode01 <tacode01@hq-tacode-001.digikala.COM>
2024-06-30 15:21:45 +0330

    adding new databes: postgres, instead of sqlite3

--- myweb/settings.py
+++ myweb/settings.py
@@ -76,12 +76,24 @@ WSGI_APPLICATION = 'myweb.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'polls',
+        'USER': 'postgres',
+        'PASSWORD': 'saman36117',
+        'HOST': 'localhost',  # Change if needed
+        'PORT': 5432,         # Default PostgreSQL port
     }
 }
+
 
 
 # Password validation
