3e52c96aa992d6797f0d6f88e72ee58f5099a814
Saman-naruee <samannaruee@gmail.com>
2024-11-15 22:31:41 +0330

    update urls.py as need in views.

--- polls/urls.py
+++ polls/urls.py
@@ -8,8 +8,8 @@ app_name = 'polls'
 urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
     path('admin/', admin.site.urls),
-    path("<int:question_id>/", views.DetailsView.as_view(), name='detail'),
-    path("<int:question_id>/results/", views.ResultsView.as_view(), name='results'),
+    path("<int:pk>/", views.DetailsView.as_view(), name='detail'),
+    path("<int:pk>/results/", views.ResultsView.as_view(), name='results'),
     path("<int:question_id>/vote/", views.vote, name='vote'),
     path("learning", views.learning, name='learning')
 ]
