1e628e256dbdb0b8c76aeec9ea083801ba1f54ca
Saman-naruee <samannaruee@gmail.com>
2024-11-19 00:29:15 +0330

    update admin site for question model.

--- myweb/settings.py
+++ myweb/settings.py
@@ -64,7 +64,7 @@ ROOT_URLCONF = 'myweb.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        "DIRS": [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -80,16 +80,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'myweb.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -100,20 +90,7 @@ DATABASES = {
         'PORT': 5432,         # Default PostgreSQL port
     }
 }
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql',
-    #     'NAME': 'events', # events
-    #     'USER': 'postgres',
-    #     'PASSWORD': 'saman36117',
-    #     'HOST': 'localhost',  # Change if needed
-    #     'PORT': 5432,         # Default PostgreSQL port
-    # }
 
-
-
-
-# Password validation
-# https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
